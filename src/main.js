c562a4daf5b4e2be472c7dff0498c9c349e054c7
zhangyushi <1193822368@qq.com>
2019-03-08 11:09:01 +0800

    day1到swipe

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import router from './router'
 import Mint from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
 Vue.use(Mint)
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import HomeContainer from '@/components/tabbar/HomeContainer'
+import MemberContainer from '@/components/tabbar/MemberContainer'
+import SearchContainer from '@/components/tabbar/SearchContainer'
+import ShopcarContainer from '@/components/tabbar/ShopCarContainer'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path: '/', redirect: '/home'},
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/home',
+      name: 'homecontainer',
+      component: HomeContainer
+    },
+    {
+      path: '/member',
+      name: 'membercontainer',
+      component: MemberContainer
+    },
+    {
+      path: '/search',
+      name: 'SearchContainer',
+      component: SearchContainer
+    },
+    {
+      path: '/shopcar',
+      name: 'shopcarcontainer',
+      component: ShopcarContainer
     }
-  ]
+  ],
+  linkActiveClass: 'mui-active'
 })
