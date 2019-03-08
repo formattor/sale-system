d7d966e7c47be280412f169b74b882cfb447f9aa
zhangyushi <1193822368@qq.com>
2019-03-08 15:23:54 +0800

    打卡

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <!-- 顶部 header 区域-->
       <mt-header fixed title="书店管理系统"></mt-header>
     <!-- 中间 router 区域-->
-      <router-view/>
+      <transition mode="out-in">
+				<router-view/>
+			</transition>
     <!-- 底部 TabBar 区域-->
     <nav class="mui-bar mui-bar-tab">
 			<router-link class="mui-tab-item" href="#tabbar" to="/home">
@@ -37,6 +39,22 @@ export default {
 
 <style scoped>
 .app-container{
-  padding-top:40px;
+	padding-top:40px;
+	overflow-x: hidden;
+}
+
+.v-enter{
+	opacity: 0;
+	transform: translateX(100%);
+}
+.v-leave-to{
+	opacity: 0;
+	transform: translateX(-100%);
+	position: absolute;
+}
+
+.v-enter-active,
+.v-leave-active{
+	transition: all 0.5s ease;
 }
 </style>
--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>Home</h3>
+    <!-- <h3>Home</h3> -->
     <mt-swipe :auto="4000">
       <!-- 后台服务器已经关掉 -->
       <!-- <mt-swipe-item v-for="(item,index) in lunbotuList" :key="index">
@@ -10,31 +10,53 @@
      <mt-swipe-item>2</mt-swipe-item>
      <mt-swipe-item>3</mt-swipe-item>
     </mt-swipe>
+    <!-- eslint-disable -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+		  <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		    <img src="../../images/menu1.png" alt="" />
+		    <div class="mui-media-body">新闻资讯</div></a></li>
+		  <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		    <img src="../../images/menu2.png" alt="" />
+		    <div class="mui-media-body">图片分享</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		    <img src="../../images/menu3.png" alt="" />
+		    <div class="mui-media-body">商品购买</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		    <img src="../../images/menu4.png" alt="" />
+		    <div class="mui-media-body">留言反馈</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		    <img src="../../images/menu5.png" alt="" />
+		    <div class="mui-media-body">视频专区</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		    <img src="../../images/menu6.png" alt="" />
+		    <div class="mui-media-body">联系我们</div></a></li>
+		</ul>
   </div>
 </template>
 
 <script>
-import {Toast} from 'mint-ui'
+// import {Toast} from 'mint-ui'
 export default {
   data () {
+    return {}
     // eslint-disable-next-line
-    lunbotuList:[]
+    // lunbotuList:[]
   },
   created () {
-    this.getLunbotu()
+    // this.getLunbotu()
   },
   methods: {
-    getLunbotu () {
-      this.$http.get('http://vue.studyit.io/api/getlunbo')
-        .then(result => {
-          // console.log(result.body)
-          if (result.body.status === 0) {
-            this.lunbotuList = result.body.message
-          } else {
-            Toast('加载轮播图失败')
-          }
-        })
-    }
+    // getLunbotu () {
+    //   this.$http.get('http://vue.studyit.io/api/getlunbo')
+    //     .then(result => {
+    //       // console.log(result.body)
+    //       if (result.body.status === 0) {
+    //         this.lunbotuList = result.body.message
+    //       } else {
+    //         Toast('加载轮播图失败')
+    //       }
+    //     })
+    // }
   }
 }
 </script>
@@ -56,6 +78,20 @@ export default {
       width: 100%;
       height: 100%;
     }
+    .mui-media-body{
+      font-size: 13px;
+    }
   }
+}
+.mui-grid-view.mui-grid-9{
+  background-color: #fff;
+  margin: none;
+  img{
+    width: 70px;
+    height: 70px;
+  }
+}
+.mui-grid-view.mui-grid-9 .mui-table-view-cell{
+  border: none;
 }
 </style>
