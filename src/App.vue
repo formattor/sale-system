981d72c876e34398cd7081bb478ccf5cfa709625
zhangyushi <1193822368@qq.com>
2019-03-08 09:13:19 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<!-- eslint-disable -->
+  <div id="app" class="app-container">
+    <!-- 顶部 header 区域-->
+      <mt-header fixed title="书店管理系统"></mt-header>
+    <!-- 中间 router 区域-->
+
+    <!-- 底部 TabBar 区域-->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+.app-container{
+  padding-top:40px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <productOne v-bind:products="products"></productOne>
+    <productTwo v-bind:products="products"></productTwo> -->
+    <h1>123</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
