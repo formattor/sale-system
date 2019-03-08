dbabedd6982a122e2a2cd705af2f96843ec2340a
zhangyushi <1193822368@qq.com>
2019-03-08 21:52:21 +0800

    haha

--- src/components/subcomponents/comments.vue
+++ src/components/subcomponents/comments.vue
@@ -5,12 +5,13 @@
     <textarea placeholder="请输入想说的内容(最多120字)" maxlength="120"></textarea>
     <mt-button type="primary" size="large">发表评论</mt-button>
     <div class="cmt-list">
-      <div class="cmt-item">
+      <div class="cmt-item" v-for="(item,i) in comments" :key="item.add_time">
         <div class="cmt-title">
-          第一楼 &nbsp;&nbsp;用户：匿名用户&nbsp;&nbsp;发表时间：2012-12-12 12：12：12
+          第{{i+1}}楼 &nbsp;&nbsp;用户：{{item.user_name}}&nbsp;&nbsp;
+          发表时间：{{item.add_time|dateFormat}}
         </div>
         <div class="cmt-body">
-          八千里路云和月
+          {{item.content==='undefined'? '此用户什么也没说':item.content}}
         </div>
       </div>
     </div>
@@ -19,8 +20,29 @@
 </template>
 
 <script>
+import {Toast} from 'mint-ui'
 export default {
-
+  data () {
+    return {
+      pageIndex: 1,
+      comments: []
+    }
+  },
+  created () {
+    this.getComments()
+  },
+  methods: {
+    getComments () {
+      this.$http.get('').then(result => {
+        if (result.body.status === 0) {
+          this.comments = result.body.message
+        } else {
+          Toast('获取评论失败')
+        }
+      })
+    }
+  },
+  props: ['id']
 }
 </script>
 
@@ -33,6 +55,20 @@ export default {
     font-size: 14px;
     height: 90px;
     margin: 0;
+  }
+}
+.cmt-list{
+  margin:5px 0;
+  .cmt-item{
+    font-size: 13px;
+    .cmt-title{
+      background-color: #ccc;
+      line-height: 30px;
+    }
+    .cmt-body{
+      line-height: 35px;
+      text-indent: 2em;
+    }
   }
 }
 </style>
